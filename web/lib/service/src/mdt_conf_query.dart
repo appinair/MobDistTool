667852e2915b32c1f9a878da40d3f620ebf82501
westhacker <bin5452@gmail.com>
2016-07-15 14:28:31 +0200

    theme

--- web/lib/service/src/mdt_conf_query.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:core';
-
-String mdtServerApiRootUrl = const String.fromEnvironment('mode') == 'javascript' ? "/api" : "http://localhost:8080/api";
-final String appVersion = "v1";
-final String appPath = "/applications/${appVersion}";
-final String artifactsPath = "/art/${appVersion}";
-final String inPath = "/in/${appVersion}";
-final String usersPath = "/users/${appVersion}";
-
